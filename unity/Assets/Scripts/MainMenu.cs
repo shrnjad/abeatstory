04c5df877b0c491a2f6ecef30394c66c2114a303
RenoRosco <Reno666@gmx.de>
2013-01-26 17:32:59 +0100

    main menu done

--- unity/Assets/Scripts/MainMenu.cs
+++ unity/Assets/Scripts/MainMenu.cs
@@ -3,6 +3,20 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour 
 {
+	[SerializeField] Transform m_MainMenuButtons;
+	[SerializeField] Transform m_HighscoreButtons;
+	[SerializeField] Transform m_CreditsButtons;
+	
+	private bool AnimateHighscoreButtons = false;
+	Vector3 HighscoreDelta;
+	Vector3 MenuDelta;
+	Vector3 TargetHighscorePos;
+	Vector3 TargetMenuPos;
+	float animTime;
+	
+	Transform oneAnim;
+	Transform twoAnim;
+	
 	public void StartGame()
 	{
 		SoundManager.Instance.SwitchToGameTheme();
@@ -12,12 +26,78 @@ public class MainMenu : MonoBehaviour
 	
 	public void LoadHighscores()
 	{
-		Debug.Log ("Load Highscore");
+		if(AnimateHighscoreButtons)
+			return;
+		
+		oneAnim = m_HighscoreButtons;
+		twoAnim = m_MainMenuButtons;
+
+		InitAnimValues();
+	}
+	
+	public void LeaveHighscores()
+	{
+		if(AnimateHighscoreButtons)
+			return;
+		
+		oneAnim = m_HighscoreButtons;
+		twoAnim = m_MainMenuButtons;
+		
+		InitAnimValues();
+	}
+	
+	public void Exit()
+	{
+		Application.Quit();
 	}
 	
 	public void ShowCredits()
 	{
-		Debug.Log ("Shoe Credits");
+		if(AnimateHighscoreButtons)
+			return;
+		
+		oneAnim = m_CreditsButtons;
+		twoAnim = m_MainMenuButtons;
+
+		InitAnimValues();
+	}
+	
+	public void LeaveCredits()
+	{
+		if(AnimateHighscoreButtons)
+			return;
+		
+		oneAnim = m_CreditsButtons;
+		twoAnim = m_MainMenuButtons;
+
+		InitAnimValues();		
+	}
+	
+	public void Update()
+	{
+		if(AnimateHighscoreButtons)
+		{
+			oneAnim.position += HighscoreDelta*Time.deltaTime*2f;
+			twoAnim.position += MenuDelta * Time.deltaTime*2f;
+			animTime -= Time.deltaTime*2f;
+			if(animTime < 0)
+			{
+				AnimateHighscoreButtons = false;
+				twoAnim.position = TargetMenuPos;
+				oneAnim.position = TargetHighscorePos;
+			}
+		}
+	}
+	
+	private void InitAnimValues()
+	{
+		TargetMenuPos = oneAnim.position;
+		TargetHighscorePos = twoAnim.position;
+		MenuDelta = TargetMenuPos - twoAnim.position;
+		HighscoreDelta = TargetHighscorePos - oneAnim.position;
+		animTime = 1f;
+		AnimateHighscoreButtons = true;
+		
 	}
 
 }
