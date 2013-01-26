b69dfa023e35a3963ee7f782d138f4e4b4d91bab
Senad Hrnjadovic <shrnjad@gmail.com>
2013-01-26 20:37:40 +0100

    Built game over dummy GUI with 3d buttons.

--- unity/Assets/Scripts/CharDeathGui.cs
+++ unity/Assets/Scripts/CharDeathGui.cs
@@ -5,9 +5,7 @@ public class CharDeathGui : MonoBehaviour {
 	
 	[SerializeField] CharacterConnection m_character;
 	
-	[SerializeField] Texture2D m_backTex;
-	[SerializeField] Texture2D m_restartTex;
-	[SerializeField] Texture2D m_quitTex;
+	[SerializeField] GameObject m_guiGroup;
 
 	// Use this for initialization
 	void Start () {
@@ -16,28 +14,32 @@ public class CharDeathGui : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		m_guiGroup.SetActive( !m_character.isAlive );
 	}
 	
 	public void OnGUI()
 	{
-		if ( m_character == null ) return;
-		
-		if( !m_character.isAlive )
-		{
-			int backWidth = 400;
-			int backHeight = 200;
-			int backX =  (int) ((Screen.width - backWidth) * .5f);
-			int backY = (int) ((Screen.height - backHeight) * .5f);
-			
-			GUI.DrawTexture( new Rect(backX, backY, backWidth, backHeight), m_backTex );
-			
-			int buttonWidth = 40;
-			int buttonHeight = 20;
-			if(GUI.Button (new Rect (backX + 10, backHeight + 10 + buttonWidth, buttonWidth, buttonHeight), m_restartTex)) {
-				Application.LoadLevel("runner 1");
-			}
-		}
+//		if ( m_character == null ) return;
+//		
+//		if( !m_character.isAlive )
+//		{
+//			int backWidth = 400;
+//			int backHeight = 200;
+//			int backX =  (int) ((Screen.width - backWidth) * .5f);
+//			int backY = (int) ((Screen.height - backHeight) * .5f);
+//			
+//			GUI.DrawTexture( new Rect(backX, backY, backWidth, backHeight), m_backTex );
+//			
+//			int buttonWidth = 40;
+//			int buttonHeight = 20;
+//			if(GUI.Button (new Rect (backX + 10, backHeight + 10 + buttonWidth, buttonWidth, buttonHeight), m_restartTex)) {
+//				Application.LoadLevel("runner 1");
+//			}
+//		}
+	}
+	
+	public void Restart() {
+		Application.LoadLevel("runner 1");
 	}
 
 }
