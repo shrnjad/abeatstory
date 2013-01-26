5cfd584485b81cafcb672278f3855b906598b57a
Senad Hrnjadovic <shrnjad@gmail.com>
2013-01-26 20:51:02 +0100

    Finished up game over gui.

--- unity/Assets/Scripts/CharDeathGui.cs
+++ unity/Assets/Scripts/CharDeathGui.cs
@@ -17,29 +17,12 @@ public class CharDeathGui : MonoBehaviour {
 		m_guiGroup.SetActive( !m_character.isAlive );
 	}
 	
-	public void OnGUI()
-	{
-//		if ( m_character == null ) return;
-//		
-//		if( !m_character.isAlive )
-//		{
-//			int backWidth = 400;
-//			int backHeight = 200;
-//			int backX =  (int) ((Screen.width - backWidth) * .5f);
-//			int backY = (int) ((Screen.height - backHeight) * .5f);
-//			
-//			GUI.DrawTexture( new Rect(backX, backY, backWidth, backHeight), m_backTex );
-//			
-//			int buttonWidth = 40;
-//			int buttonHeight = 20;
-//			if(GUI.Button (new Rect (backX + 10, backHeight + 10 + buttonWidth, buttonWidth, buttonHeight), m_restartTex)) {
-//				Application.LoadLevel("runner 1");
-//			}
-//		}
-	}
-	
 	public void Restart() {
 		Application.LoadLevel("runner 1");
+	}
+	
+	public void Quit() {
+		Application.LoadLevel("Menu");
 	}
 
 }
