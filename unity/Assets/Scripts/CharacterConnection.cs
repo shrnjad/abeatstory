1b244e55b0999beafc59c8808a9768bba2418935
Senad Hrnjadovic <shrnjad@gmail.com>
2013-01-26 13:36:28 +0100

    Removed debug output.

--- unity/Assets/Scripts/CharacterConnection.cs
+++ unity/Assets/Scripts/CharacterConnection.cs
@@ -61,7 +61,6 @@ public class CharacterConnection : MonoBehaviour {
 			// Cap at 0.
 			m_heartHightValue = Mathf.Max( 0, m_heartHightValue );
 		}
-		Debug.Log( "" + m_heartHightValue );
 
 		if(m_lowTimer > 0)
 		{
